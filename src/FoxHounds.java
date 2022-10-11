7668f04d36c5723a5d127856aeb4a0458e89c8f8
Gábor Krisztián <gaborkiki16@gmail.com>
2022-10-11 19:07:58 +0200

    Comm

--- src/FoxHounds.java
+++ src/FoxHounds.java
@@ -1,5 +1,7 @@
 public class FoxHounds {
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
+
+        System.out.println("Another one!!!");
     }
 }
